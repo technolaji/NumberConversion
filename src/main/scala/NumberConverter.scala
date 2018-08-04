0c5540bfc575fa657e731109e964b8983a5e9510
bolaji.abiodun <bolaji.abiodun@just-eat.com>
2018-08-04 16:12:19 +0100

    read args from cmd

---
+++ src/main/scala/NumberConverter.scala
@@ -0,0 +1,8 @@
+object NumberConverter extends App {
+
+  override def main(args: Array[String]): Unit = {
+    val number = scala.io.StdIn.readLine("Please enter the number you want converted")
+
+  }
+
+}
