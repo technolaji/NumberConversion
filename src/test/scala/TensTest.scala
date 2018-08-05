9df6f184073451446e2e3d6e067aa75c9fdb0dba
bolaji.abiodun <bolaji.abiodun@just-eat.com>
2018-08-06 00:27:22 +0100

    test cases for two digit numbers

--- src/test/scala/TensTest.scala
+++ src/test/scala/TensTest.scala
@@ -29,5 +29,39 @@ class TensTest extends FunSuite{
 
   }
 
+  test("WHEN input is 20 THEN function should return twenty") {
+    val input = 20
+    val actualResult = tens.get(input)
+    val expectedResult = "twenty "
+    assert(actualResult == expectedResult)
+  }
+
+  test("WHEN input is 26 THEN function should return twenty six") {
+    val input = 26
+    val actualResult = tens.get(input)
+    val expectedResult = "twenty six"
+    assert(actualResult == expectedResult)
+  }
+
+  test("WHEN input is 50 THEN function should return fifty") {
+    val input = 50
+    val actualResult = tens.get(input)
+    val expectedResult = "fifty "
+    assert(actualResult == expectedResult)
+  }
+
+  test("WHEN input is 60 THEN function should return sixty") {
+    val input = 60
+    val actualResult = tens.get(input)
+    val expectedResult = "sixty "
+    assert(actualResult == expectedResult)
+  }
+
+  test("WHEN input is 78 THEN function should return seventy eight") {
+    val input = 78
+    val actualResult = tens.get(input)
+    val expectedResult = "seventy eight"
+    assert(actualResult == expectedResult)
+  }
 
 }
