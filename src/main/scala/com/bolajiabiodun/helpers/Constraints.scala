0425246f347fa1428a2baab871aa05fdd98edad4
bolaji.abiodun <bolaji.abiodun@just-eat.com>
2018-08-05 13:41:10 +0100

    Added tests cases for decimal numbers and making sure number is less than a billion

--- src/main/scala/com/bolajiabiodun/helpers/Constraints.scala
+++ src/main/scala/com/bolajiabiodun/helpers/Constraints.scala
@@ -1,11 +1,15 @@
 package com.bolajiabiodun.helpers
 
-class Constraints{
+class Constraints(){
 
-  def isNumber(x: Int ) : Boolean = {
-    x.isValidInt
+
+  def isNumber(x: Any ) : Boolean = {
+    x.isInstanceOf[Integer]
   }
 
+  def isLessThanOneBillion(x:Int) : Boolean = {
+    x < 1000000000
+  }
 }
 
 
--- src/test/scala/helpersTest/ConstraintsTest.scala
+++ src/test/scala/helpersTest/ConstraintsTest.scala
@@ -17,11 +17,26 @@ class ConstraintsTest extends FunSuite {
   }
 
   test("WHEN argument is a character THEN method should return false "){
-    val arg = "arg"
+    val arg = "testArg"
     val actualResult = constraints.isNumber(arg)
-    val expectedResult = true
+    val expectedResult = false
     assert(actualResult == expectedResult)
   }
+
+  test("WHEN argument contains decimals THEN method should return false "){
+    val arg = 3.5
+    val actualResult = constraints.isNumber(arg)
+    val expectedResult = false
+    assert(actualResult == expectedResult)
+  }
+
+  test("WHEN number is greater than 999,999,999 THEN method should return false "){
+    val arg = 1000000000
+    val actualResult = constraints.isLessThanOneBillion(arg)
+    val expectedResult = false
+    assert(actualResult == expectedResult)
+  }
+
 
 
 }
