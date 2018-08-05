26e2542721e17922b3ab4797032c3323b82659e8
bolaji.abiodun <bolaji.abiodun@just-eat.com>
2018-08-05 15:25:48 +0100

    Added method to check assumptions

--- src/main/scala/com/bolajiabiodun/NumberConverter.scala
+++ src/main/scala/com/bolajiabiodun/NumberConverter.scala
@@ -1,10 +1,17 @@
 package com.bolajiabiodun
 
+import com.bolajiabiodun.helpers.Constraints
+
 
 object NumberConverter extends App {
 
   override def main(args: Array[String]): Unit = {
-    val number = scala.io.StdIn.readLine("Please enter the number you want converted")
+    val rawNumber = scala.io.StdIn.readLine("Please enter the number you want converted \n")
+
+    val constraints = new Constraints
+    val number = constraints.checkAssumptions(rawNumber)
+
+    print(number)
   }
 
 
