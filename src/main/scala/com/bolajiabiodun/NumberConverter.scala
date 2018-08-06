e22ec05a48b1a4394417a267775222cf42ad5a5c
bolaji.abiodun <bolaji.abiodun@just-eat.com>
2018-08-06 04:02:24 +0100

    Implemented test cases for NumberConverter

--- src/main/scala/com/bolajiabiodun/NumberConverter.scala
+++ src/main/scala/com/bolajiabiodun/NumberConverter.scala
@@ -12,17 +12,29 @@ object NumberConverter extends App {
     val numberOfDigits = rawNumber.length
 
     val constraints = new Constraints
+
+
     val number = constraints.checkAssumptions(rawNumber)
 
     val units = new Units
     val tens = new Tens
     val hundreds = new Hundreds
+    val thousands = new Thousands
+    val millions = new Millions
 
     numberOfDigits match {
-      case 3 => print(hundreds.get(number))
-      case 2 => print(tens.get(number))
-      case 1 => print(units.get(number))
+
+      case 9 => print(millions.getWords(number))
+      case 8 => print(millions.getWords(number))
+      case 7 => print(millions.getWords(number))
+      case 6 => print(thousands.getWords(number))
+      case 5 => print(thousands.getWords(number))
+      case 4 => print(thousands.getWords(number))
+      case 3 => print(hundreds.getWords(number))
+      case 2 => print(tens.getWords(number))
+      case 1 => print(units.getWords(number))
     }
+
   }
 
 
